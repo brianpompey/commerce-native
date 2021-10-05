9b2e6c7404695bcca9019114261d87619e54ec80
brianpompey <brianthescholar@gmail.com>
2021-10-05 11:30:08 -0400

    cleaning up

--- screens/HomePage.js
+++ screens/HomePage.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import Icon from '../components/Icon';
 import { Main } from './Main.js';
 import { ProductsList } from './ProductsList.js';
 import { CartIcon } from './../components/CartIcon.js';
@@ -24,20 +22,3 @@ export default function HomePage() {
 }
 
 
-const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    headingText: {
-      fontSize: 30,
-      color: '#FF0000',
-    },
-    stretch: {
-        width: 250,
-        height: 200,
-        resizeMode: 'stretch',
-    }
-});
