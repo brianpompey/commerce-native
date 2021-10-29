8b167e249638d96798a4c47ee77a2f26fe63ec7e
brianpompey <brianthescholar@gmail.com>
2021-10-29 00:29:37 -0400

    .

--- services/ProductsService.js
+++ services/ProductsService.js
@@ -4,21 +4,21 @@ const PRODUCTS = [
         name: 'Weber',
         price: 110,
         image: require('../assets/smokers/weber.jpg'),
-        description: 'A headset combines a headphone with microphone. Headsets are made with either a single-earpiece (mono) or a double-earpiece (mono to both ears or stereo).'
+        description: 'The best starter grill.'
     },
     {
         id: 101,
         name: 'Traeger',
         price: 600,
         image: require('../assets/smokers/traeger.png'),
-        description: 'A model car, or toy car, is a miniature representation of an automobile. Other miniature motor vehicles, such as trucks, buses, or even ATVs, etc. are often included in this general category.'
+        description: 'Automatic, efficient, reliable'
     },
     {
         id: 102,
         name: 'Kamado Joe',
         price: 800,
         image: require('../assets/smokers/kamado.jpg'),
-        description: 'A cupcake (also British English: fairy cake; Hiberno-English: bun; Australian English: fairy cake or patty cake[1]) is a small cake designed to serve one person.'
+        description: 'A big splurge. But Quality Unmatched'
     }
 ];
 
